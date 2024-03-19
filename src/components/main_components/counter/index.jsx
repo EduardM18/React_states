61fd444935823858191cbc9ea9f23ea07c0216a9
Eduard <edo@gmail.com>
2024-03-19 14:48:51 +0400

    Finished: Counter

--- src/components/main_components/counter/index.jsx
+++ src/components/main_components/counter/index.jsx
@@ -3,12 +3,35 @@ import "./style.css";
 
 
 class Counter extends Component {
-  render(){
-    return (
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  } 
+  render() {
+    return(
       <>
-        <h1>Hello</h1>
+        <h1 id="counter_heading">Counter</h1>
+        <div id="counter_container">
+          <p id="p_counter">{this.state.count}</p>
+
+          <div id="counter_btns_container">
+            <button className="counter_btn" onClick={() =>{
+              this.setState({
+                count: this.state.count +1
+              })
+            }}>increment</button>
+
+            <button className="counter_btn" disabled = {this.state.count === 0} onClick={() =>{
+              this.setState({
+                count: this.state.count -1
+              })
+            }}>decrement</button>
+          </div>
+        </div>
       </>
-    );
+    )
   }
 }
 
