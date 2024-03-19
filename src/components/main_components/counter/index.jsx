ec87437ce5cf69525f4c8647a83f2916adf487ce
Eduard <edo@gmail.com>
2024-03-19 13:56:54 +0400

    Add: components folder

---
+++ src/components/main_components/counter/index.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./style.css";
+
+
+class Counter extends Component {
+  render(){
+    return (
+      <>
+        <h1>Hello</h1>
+      </>
+    );
+  }
+}
+
+export default Counter;
