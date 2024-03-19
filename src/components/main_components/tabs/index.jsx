aa3470d1bb76fc9ce1fedee73719a387701bb9b0
Eduard <edo@gmail.com>
2024-03-19 19:20:08 +0400

    Fix: bugs

--- src/components/main_components/tabs/index.jsx
+++ src/components/main_components/tabs/index.jsx
@@ -4,35 +4,31 @@ import Counter from "../counter";
 import ColorPicker from "../color_picker";
 import Accordion from "../accordion";
 
+
 class Tabs extends Component {
   constructor(props){
     super(props);
     this.state = {
       tabs: [<Counter />,<ColorPicker />,<Accordion />],
-      current_tab: <Counter />
+      current_tab: <Counter />,
+      sections: ["Counter","Color picker","Accordion"]
     };
   } 
   render() {
     return(
       <>
         <div id="sections_container">
-            <p id="current_section" className="section" onClick={() =>{
-              this.setState({
-                current_tab: this.state.tabs[0]
-              })
-            }}>Counter</p>
-
-            <p className="section" onClick={() =>{
-              this.setState({
-                current_tab: this.state.tabs[1]
-              })
-            }}>Color picker</p>
-            
-            <p className="section"  onClick={() =>{
-              this.setState({
-                current_tab: this.state.tabs[2]
-              })
-            }}>Accordion</p>
+          {this.state.sections.map((section,i)=>{
+              return(
+                <p className="section" key={`section_${i}`} style={this.state.tabs[i] === this.state.current_tab ? {textDecoration: "underline"} : {textDecoration: "none"}} onClick={() =>{
+                  this.setState({
+                    current_tab: this.state.tabs[i]
+                  })
+                  
+                }}>{section}</p>
+              )
+            })
+          }
         </div>
         {this.state.current_tab}
       </>
